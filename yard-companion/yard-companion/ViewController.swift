fe4aae458e44950284c8fe0e8687fe56d6814f2c
James Touri <j_touri@yahoo.com>
2018-02-03 13:00:44 -0800

    Add files via upload

---
+++ yard-companion/yard-companion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yard-companion
+//
+//  Created by James touri on 2/3/18.
+//  Copyright © 2018 yard-companion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
