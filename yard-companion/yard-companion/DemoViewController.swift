d88094a62c3cda4ca4724d4bcd116f037eac74ae
James Touri <j_touri@yahoo.com>
2018-02-03 21:03:45 -0800

    Got camera working, now focusing on upload selfie

--- yard-companion/yard-companion/DemoViewController.swift
+++ yard-companion/yard-companion/DemoViewController.swift
@@ -7,29 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
+
 
 class DemoViewController: UIViewController {
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        previewView.layer.addSublayer(videoPreviewLayer!)
+        captureSession?.startRunning()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
 }
---
+++ yard-companion/yard-companion/FacesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FacesCollectionViewCell.swift
+//  yard-companion
+//
+//  Created by James touri on 2/3/18.
+//  Copyright © 2018 yard-companion. All rights reserved.
+//
+
+import UIKit
+
+class FacesCollectionViewCell: UICollectionViewCell {
+    
+}
